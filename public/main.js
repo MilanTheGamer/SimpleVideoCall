bbba60b8810fde8fe71e3199ee92abe0b4014dad
Milan Salim <milan.salim@zeazonz.com>
2020-02-20 08:57:57 +0530

    only google stun server

--- public/main.js
+++ public/main.js
@@ -14,7 +14,7 @@ navigator.mediaDevices.getUserMedia({ video:true, audio:true })
 
     // used to initialize a peer
     function InitPeer(type){
-        let peer = new Peer({initiator:(type == "init") ? true : false, stream:stream, trickle:false})
+        let peer = new Peer({initiator:(type == "init") ? true : false, config: { iceServers: { urls: 'stun:stun.l.google.com:19302' } }, stream:stream, trickle:false})
         peer.on("stream", stream => {
             CreateVideo(stream)
         });

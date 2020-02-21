6e1de4b56c75edd0b68143f2c2ab35af484bbd23
Milan Salim <milan.salim@zeazonz.com>
2020-02-21 10:02:53 +0530

    private stun/turn server

--- public/main.js
+++ public/main.js
@@ -17,21 +17,32 @@ navigator.mediaDevices.getUserMedia({ video:true, audio:true })
         let peer = new Peer({initiator:(type == "init") ? true : false, config: { 
             iceServers: [
                 // Stun Servers
-                { urls: 'stun:stun.l.google.com:19302' },
-                { url: 'stun:stun1.l.google.com:19302' },
-                { url: 'stun:stun2.l.google.com:19302' },
+                // { urls: 'stun:stun.l.google.com:19302' },
+                // { url: 'stun:stun1.l.google.com:19302' },
+                // { url: 'stun:stun2.l.google.com:19302' },
                 // Turn Servers
                 {
-                    url: 'turn:192.158.29.39:3478?transport=udp',
-                    credential: 'JZEOEt2V3Qb0y27GRntt2u2PAYA=',
-                    username: '28224511:1379330808'
+                    url:"stun:numb.viagenie.ca",
+                    username:"milanthegamer@gmail.com",
+                    credential:"Milan@123"
+                },
+                {
+                    url:"turn:numb.viagenie.ca",
+                    username:"milanthegamer@gmail.com",
+                    credential:"Milan@123"
                 },
 
-                {
-                    url: 'turn:192.158.29.39:3478?transport=udp',
-                    credential: 'JZEOEt2V3Qb0y27GRntt2u2PAYA=',
-                    username: '28224511:1379330808'
-                },
+                // {
+                //     url: 'turn:192.158.29.39:3478?transport=udp',
+                //     credential: 'JZEOEt2V3Qb0y27GRntt2u2PAYA=',
+                //     username: '28224511:1379330808'
+                // },
+
+                // {
+                //     url: 'turn:192.158.29.39:3478?transport=udp',
+                //     credential: 'JZEOEt2V3Qb0y27GRntt2u2PAYA=',
+                //     username: '28224511:1379330808'
+                // },
                 ] 
             }, stream:stream, trickle:false, iceTransportPolicy: 'relay', reconnectTimer: 3000})
         peer.on("stream", stream => {
